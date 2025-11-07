62eec948372cec42c80d52ad8a6918baf490a48a
prkd69 <prkd2002@gmail.com>
2025-11-07 11:14:02 +0100

    utils added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rusty",
+    author_email="kanarusty2002@gmail.com",
+    packages=find_packages()
+)
